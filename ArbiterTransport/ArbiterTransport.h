cf8a6f52895f9211fe9c58c28fecc7706995aad7
zZED <xingxing0468@hotmail.com>
2014-11-20 13:54:57 +0800

    init

---
+++ ArbiterTransport/ArbiterTransport.h
@@ -0,0 +1,26 @@
+#ifndef _ARBITER_TRANSPORT_H
+#define _ARBITER_TRANSPORT_H
+
+namespace ArbiterTransport
+{
+class ArbiterTransport
+{
+public:
+
+
+private:
+
+
+potected:
+
+
+};
+
+
+}
+
+
+
+
+
+#endif
