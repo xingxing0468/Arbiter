2b9e15b0504577999a1e53a72e95f21e057b8d23
zZED <xingxing0468@hotmail.com>
2014-11-20 13:41:41 +0800

    init

---
+++ ArbiterDataService/ArbiterDataService.h
@@ -0,0 +1,29 @@
+#ifndef _ARBITER_DATA_SERVICE_H
+#define _ARBITER_DATA_SERVICE_H
+
+namespace ArbiterDataService
+{
+class DataService
+{
+public:
+
+
+private:
+
+
+protected
+
+
+};
+
+}
+
+
+
+
+
+
+
+
+
+#endif
